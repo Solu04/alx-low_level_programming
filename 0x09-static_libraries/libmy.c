541674e6da73a658faead8de9878ece92b4ae8cc
Solu04 <solutionresource15@gmail.com>
2023-03-10 03:36:00 -0800

    Fixing error

---
+++ 0x09-static_libraries/libmy.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+	/* function definition */
+}
+
+int _islower(int c)
+{
+	/* function definition */
+}
+
+int _isalpha(int c)
+{
+	/* function definition */
+}
+
+int _abs(int n)
+{
+	/* function definition */
+}
+
+int _isupper(int c)
+{
+	/* function definition */
+}
+
+int _isdigit(int c)
+{
+	/* function definition */
+}
+
+int _strlen(char *s)
+{
+	/* function definition */
+}
+
+void _puts(char *s)
+{
+	/* function definition */
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	/* function definition */
+}
+
+int _atoi(char *s)
+{
+	/* function definition */
+}
+
+char *_strcat(char *dest, char *src)
+{
+	/* function definition */
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	/* function definition */
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	/* function definition */
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	/* function definition */
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/* function definition */
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	/* function definition */
+}
+
+char *_strchr(char *s, char c)
+{
+	/* function definition */
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	/* function definition */
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+	/* function definition */
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+	/* function definition */
+}
